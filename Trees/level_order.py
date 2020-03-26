8e5ee22f648d7b778f29a74596b2234858fbea24
Julie Nguyen <julie-nguyen@live.com>
2020-03-26 14:58:01 -0400

    implemented level order traversal

--- Trees/level_order.py
+++ Trees/level_order.py
@@ -1,8 +1,15 @@
-from binary_search_tree import BinarySearchTree
+from binary_search_tree import *
 
 
 def level_order_traversal(t):
-  
+  node_items = []
+  current_level_nodes = [t.root]
+  for node in current_level_nodes:
+    if (node != None):
+      node_items.append(node.item)
+      current_level_nodes.append(node.get_left())
+      current_level_nodes.append(node.get_right())
+  return node_items
 
 
 tree = BinarySearchTree()
@@ -12,3 +19,8 @@ tree.add_leaf(40)
 tree.add_leaf(5)
 tree.add_leaf(20)
 tree.add_leaf(35)
+print(level_order_traversal(tree))
+tree2 = BinarySearchTree()
+print(level_order_traversal(tree2))
+
+
