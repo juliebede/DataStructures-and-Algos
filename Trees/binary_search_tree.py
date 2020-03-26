bbcda3de1effbce4943f7c45658405c4c6f5c0f8
Julie Nguyen <julie-nguyen@live.com>
2020-03-25 20:15:25 -0400

    initialized bst

---
+++ Trees/binary_search_tree.py
@@ -0,0 +1,13 @@
+class Node():
+
+  def __init__(self, item, left, right):
+    self.item = item
+    self.left = left
+    self.right = right
+
+class BinarySearchTree():
+
+  def __init__(self):
+    self.root = None
+
+
