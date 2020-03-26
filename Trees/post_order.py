19d4ee350f24c8b09bd22e5efa74a2251f42ac7b
Julie Nguyen <julie-nguyen@live.com>
2020-03-26 16:34:54 -0400

    saved a post order traversal file for later

---
+++ Trees/post_order.py
@@ -0,0 +1,18 @@
+from binary_search_tree import *
+
+# def post_order_traversal(node):
+#   if (node == None):
+#     return 
+#   else:
+#     return [node.item]
+
+# tree = BinarySearchTree()
+# tree.add_leaf(30)
+# tree.add_leaf(10)
+# tree.add_leaf(40)
+# tree.add_leaf(5)
+# tree.add_leaf(20)
+# tree.add_leaf(35)
+# print(post_order_traversal(tree))
+# tree2 = BinarySearchTree()
+# print(post_order_traversal(tree2))
