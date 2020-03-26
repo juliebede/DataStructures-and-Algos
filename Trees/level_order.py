c35d052f4c9fab89e44bad099b811ba0664af7b0
Julie Nguyen <julie-nguyen@live.com>
2020-03-26 14:18:22 -0400

    created a tree for testing

---
+++ Trees/level_order.py
@@ -0,0 +1,14 @@
+from binary_search_tree import BinarySearchTree
+
+
+def level_order_traversal(t):
+  
+
+
+tree = BinarySearchTree()
+tree.add_leaf(30)
+tree.add_leaf(10)
+tree.add_leaf(40)
+tree.add_leaf(5)
+tree.add_leaf(20)
+tree.add_leaf(35)
