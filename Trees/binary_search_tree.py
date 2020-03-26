2de3fe361abca22dbbaaffe94c125de1667414ce
Julie Nguyen <julie-nguyen@live.com>
2020-03-26 14:12:05 -0400

    implemented adding a leaf in binary search tree

--- Trees/binary_search_tree.py
+++ Trees/binary_search_tree.py
@@ -4,6 +4,17 @@ class Node():
     self.item = item
     self.left = left
     self.right = right
+  
+   
+  def __str__(self):
+    return f'{self.item}'
+  
+  def get_left(self):
+    return self.left
+
+  def get_right(self):
+    return self.right
+
 
 class BinarySearchTree():
 
@@ -12,21 +23,43 @@ class BinarySearchTree():
 
   def is_empty(self):
     return self.root == None
+  
+  def __str__(self):
+    return f'{self.root.item}'
+
+  def get_root(self):
+    return self.root
 
   def add_leaf(self, item):
-    if (self.is_empty):
+    if (self.is_empty()):
       self.root = Node(item, None, None)
     else:
-
-      # change the loop to have a different condition
-      # if current left or current right are None do something
       current = self.root
-      new_node = Node(item, None, None)
-      while (current != None):
+      added = False
+      while (added == False):
         if (item > current.item):
           if (current.right == None):
-            current.right = new_node
-        elif (item < current.item):
+            current.right = Node(item, None, None)
+            added = True
+          else:
+            current = current.right
+        else:
           if (current.left == None):
-            current.left =new_node
-        
+            current.left = Node(item, None, None)
+            added = True
+          else:
+           current = current.left
+          
+B = BinarySearchTree()
+# print(B.is_empty())
+B.add_leaf(9)
+print(B.is_empty())
+node1 = B.get_root()
+node1 = node1.get_left()
+B.add_leaf(5)
+node1 = B.get_root()
+node1 = node1.get_left()
+print(node1)
+B.add_leaf(10)
+node1 = B.get_root().get_right()
+print(node1)
