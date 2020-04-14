b1b1365f1f1ae0bf6b9dbae2147c527c11c5c66b
Julie Nguyen <julie-nguyen@live.com>
2020-04-14 17:28:47 -0400

    fixed bug

--- Sorting/quicksort.py
+++ Sorting/quicksort.py
@@ -12,13 +12,12 @@ def partition(arr, left, right):
 
 def quicksort(arr, left, right):
   index = partition(arr, left, right)
-  print(left, index)
   if (left < index):
     quicksort(arr, left, index - 1)
   if (right > index):
     quicksort(arr, index + 1, right)
 
 
-array = [9,2,23,2,2,4,21,443,2,21,68,33] 
+array = [9,2,23,2,2,4,21,443,2,21,68,500] 
 quicksort(array, 0, len(array) - 1)
 print(array)
