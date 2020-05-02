76e281f5e791f421ffa94de458f6bbd8a8d00204
Julie Nguyen <julie-nguyen@live.com>
2020-05-02 12:39:51 -0400

    reimplemneted quicksort

--- Sorting/quicksort.py
+++ Sorting/quicksort.py
@@ -1,21 +1,28 @@
 def partition(arr, left, right):
-  pos = left
-  while (pos <= right):
-    if (arr[pos] <= arr[right]):
-      temp = arr[left]
-      arr[left] = arr[pos]
-      arr[pos] = temp
-      left += 1
-    pos += 1
+  pivot = arr[(left + right) // 2]
+  while (left <= right):
 
-  return left - 1
+    while (arr[left] < pivot):
+      left += 1
+    
+    while (arr[right] > pivot):
+      right -= 1
+
+    if (left <= right):
+      temp = arr[left]
+      arr[left] = arr[right]
+      arr[right] = temp
+      left += 1
+      right -= 1
+    
+  return left
 
 def quicksort(arr, left, right):
   index = partition(arr, left, right)
-  if (left < index):
+  if (left < index - 1):
     quicksort(arr, left, index - 1)
-  if (right > index):
-    quicksort(arr, index + 1, right)
+  if (index < right):
+    quicksort(arr, index, right)
 
 
 array = [9,2,23,2,2,4,21,443,2,21,68,500] 
